2dad5669ff6ba627c9fa0b1350d2a2039cf48d22
adsionli <531020509@qq.com>
2018-05-02 12:11:11 +0800

    ljq

--- src/components/profit/profit.vue
+++ src/components/profit/profit.vue
@@ -1,5 +1,52 @@
 <template>
-  <div>收益情况</div>
+  <div style="margin-top:40px">
+    <el-row :gutter="20" class="mgb20">
+        <el-col :span="6">
+            <el-card shadow="hover" :body-style="{padding: '0px'}">
+                <div class="grid-content grid-con-1">
+                    <i class="el-icon-view grid-con-icon"></i>
+                    <div class="grid-cont-right">
+                        <div class="grid-num">15单</div>
+                        <div>有效订单数量</div>
+                    </div>
+                </div>
+            </el-card>
+        </el-col>
+        <el-col :span="6">
+            <el-card shadow="hover" :body-style="{padding: '0px'}">
+                <div class="grid-content grid-con-2">
+                    <i class="el-icon-message grid-con-icon"></i>
+                    <div class="grid-cont-right">
+                        <div class="grid-num">169元</div>
+                        <div>金额收益</div>
+                    </div>
+                </div>
+            </el-card>
+        </el-col>
+        <el-col :span="6">
+            <el-card shadow="hover" :body-style="{padding: '0px'}">
+                <div class="grid-content grid-con-3">
+                    <i class="el-icon-goods grid-con-icon"></i>
+                    <div class="grid-cont-right">
+                        <div class="grid-num">60件</div>
+                        <div>销售商品数量</div>
+                    </div>
+                </div>
+            </el-card>
+        </el-col>
+        <el-col :span="6">
+            <el-card shadow="hover" :body-style="{padding: '0px'}">
+                <div class="grid-content grid-con-3">
+                    <i class="el-icon-goods grid-con-icon"></i>
+                    <div class="grid-cont-right">
+                        <div class="grid-num">↑15%</div>
+                        <div>较昨日收益情况比较</div>
+                    </div>
+                </div>
+            </el-card>
+        </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -9,5 +56,88 @@ export default {
 </script>
 
 <style>
-
+    .el-row {
+        margin-bottom: 20px;
+    }
+    .grid-content {
+        display: flex;
+        align-items: center;
+        height: 100px;
+    }
+    .grid-cont-right {
+        flex: 1;
+        text-align: center;
+        font-size: 12px;
+        color: #999;
+    }
+    .grid-num {
+        font-size: 30px;
+        font-weight: bold;
+    }
+    .grid-con-icon {
+        font-size: 50px;
+        width: 100px;
+        height: 100px;
+        text-align: center;
+        line-height: 100px;
+        color: #fff;
+    }
+    .grid-con-1 .grid-con-icon {
+        background: rgb(45, 140, 240);
+    }
+    .grid-con-1 .grid-num {
+        color: rgb(45, 140, 240);
+    }
+    .grid-con-2 .grid-con-icon {
+        background: rgb(100, 213, 114);
+    }
+    .grid-con-2 .grid-num {
+        color: rgb(45, 140, 240);
+    }
+    .grid-con-3 .grid-con-icon {
+        background: rgb(242, 94, 67);
+    }
+    .grid-con-3 .grid-num {
+        color: rgb(242, 94, 67);
+    }
+    .user-info {
+        display: flex;
+        align-items: center;
+        padding-bottom: 20px;
+        border-bottom: 2px solid #ccc;
+        margin-bottom: 20px;
+    }
+    .user-avator {
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;
+    }
+    .user-info-cont {
+        padding-left: 50px;
+        flex: 1;
+        font-size: 14px;
+        color: #999;
+    }
+    .user-info-cont div:first-child {
+        font-size: 30px;
+        color: #222;
+    }
+    .user-info-list {
+        font-size: 14px;
+        color: #999;
+        line-height: 25px;
+    }
+    .user-info-list span {
+        margin-left: 70px;
+    }
+    .mgb20 {
+        margin-bottom: 20px;
+    }
+    .todo-item {
+        font-size: 14px;
+    }
+    .todo-item-del {
+        text-decoration: line-through;
+        color: #999;
+    }
 </style>
